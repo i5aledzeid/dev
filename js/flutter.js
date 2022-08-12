c2e2f2f609e0cff47829d590d6201bdb2d18abe3
Khaled Zeid <107222642+i5aledzeid@users.noreply.github.com>
2022-08-13 01:56:55 +0200

    Add files via upload

--- js/flutter.js
+++ js/flutter.js
@@ -1,17 +1,17 @@
 // Fri Jun 17 2022 11:27:28 GMT+0100 (British Summer Time)
-const date = new Date();
-print(date);
+// const date = new Date();
+// print(date);
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
+// let day = date.getDate();
+// let month = date.getMonth() + 1;
+// let year = date.getFullYear();
 
 // This arrangement can be altered based on how we want the date's format to appear.
-let currentDate = '${day}-${month}-${year}';
-console.log(currentDate); // "17-6-2022"
+// let currentDate = '${day}-${month}-${year}';
+// console.log(currentDate); // "17-6-2022"
 
-let localDate = new Date().toLocaleDateString();
-console.log(localDate); // 6/17/2022
+// let localDate = new Date().toLocaleDateString();
+// console.log(localDate); // 6/17/2022
 
 const dateToday = new Date();
 // var copyrightDate = dateToday.getFullYear()+'-'+(dateToday.getMonth()+1)+'-'+dateToday.getDate();
